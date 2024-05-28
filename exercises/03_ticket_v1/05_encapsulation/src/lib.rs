262706d7fe00b500e9a97dc1c11b0237702d7366
Mislav Markovic <mislav.markovic95@gmail.com>
2024-05-28 22:12:06 +0200

    Continue progressing trough module `03`

--- exercises/03_ticket_v1/05_encapsulation/src/lib.rs
+++ exercises/03_ticket_v1/05_encapsulation/src/lib.rs
@@ -30,10 +30,18 @@ pub mod ticket {
             }
         }
 
-        // TODO: Add three public methods to the `Ticket` struct:
-        //  - `title` that returns the `title` field.
-        //  - `description` that returns the `description` field.
-        //  - `status` that returns the `status` field.
+        pub(crate) fn title(&self) -> &str {
+            &self.title
+        }
+
+        pub(crate) fn status(&self) -> &str {
+            &self.status
+        }
+
+        pub(crate) fn description(&self) -> &str {
+            &self.description
+        }
+
     }
 }
 
